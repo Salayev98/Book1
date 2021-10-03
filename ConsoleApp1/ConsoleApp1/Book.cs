e72bd17e4eb27bb0faf630e9ecaec35af2c02d38
Salayev98 <87970921+Salayev98@users.noreply.github.com>
2021-10-04 01:08:33 +0400

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Book
+    {
+        public static int Count = 0;
+        public string Code;
+        public static string Name;
+        public static string AuthorName;
+        public static int PageCount;
+
+        public Book( string name, string authorName, int pageCount)
+        {
+            Code = Name.Substring(0, 2).ToUpper() + Count;
+            Name = name;
+            AuthorName = authorName;
+            PageCount = pageCount;
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Library.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Library 
+    {
+        public List<Book> Books;
+        public void AddList(string name, string authorName, int pageCount)
+        {
+            Book books = new Book(name, authorName, pageCount);
+            Books.Add(books);
+        }
+        public void FindAllBooksByName(string word) 
+        {
+            Books.FindAll(Book => Book.Name.Contains(word));
+        }
+        public void RemoveAllBookByName(string word) 
+        {
+            Books.RemoveAll(Book => Book.Name.Contains(word));
+        }
+        public void SearchBooks(string value) 
+        {
+            Books.FindAll(Book => Book.Name == value);
+            Books.FindAll(Book => Book.AuthorName == value);
+            Books.FindAll(Book =>
+            {
+                return Book.PageCount == int.Parse(value);
+            });
+        }
+        public void FindAllBooksByPageCountRange(int mincount,int maxcount) 
+        {
+            Books.FindAll(Books => mincount < Book.PageCount && maxcount > Book.PageCount);
+        }
+        public void RemoveByNo(string code) 
+        {
+            Books.RemoveAll(Book => Book.Code == code);
+        }
+        public void List()
+        {
+            foreach (var item in Books)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
